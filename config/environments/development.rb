66e66cccf40aa3deb036421dbbb1e884ea210377
giangmt <maitruonggiangbkhn@gmail.com>
2018-06-25 20:33:31 +0700

    devise confirmation mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,17 +28,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {host: ""}
-  config.action_mailer.smtp_settings = {
-    address: "",
-    port: 587,
-    user_name: "",
-    password: "",
-    domain: "",
-    enable_starttls_auto: true
-  }
 
   config.action_mailer.perform_caching = false
 
@@ -62,4 +51,15 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            ENV["EMAIL_USER"],
+    password:             ENV["EMAIL_PASS"],
+    authentication:       "plain",
+    enable_starttls_auto: true  }
 end
