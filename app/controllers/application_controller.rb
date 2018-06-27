cf9cedd7b9ac9d194f74f6fa7d9a03d85814c8e4
giangmt <maitruonggiangbkhn@gmail.com>
2018-06-27 11:33:01 +0700

    I18n Vietnamese

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :load_search
+  before_action :load_search, :set_locale
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path, alert: exception.message
@@ -18,9 +18,17 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   private
 
   def load_search
     @search = Movie.ransack(params[:q])
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,39 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root "static_page#home"
-  
-  get "search", to: "static_page#search"
 
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
-  resources :users do
-    member do
-      get :following, :followers
+  scope "(:locale)", locale: /en|vi/ do
+    root to: "static_page#home"
+    
+    get "search", to: "static_page#search"
+    
+    resources :users do
+      member do
+        get :following, :followers
+      end
     end
-  end
 
-  resources :relationships, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
 
-  resources :movies do
-    collection do
-      get :top
+    resources :movies do
+      collection do
+        get :top
+      end
+      resources :reviews
     end
-    resources :reviews
+    
+    namespace :admin do
+      resources :users
+      resources :movies
+      resources :reviews
+      resources :comments
+      resources :genres
+      resources :descriptions
+    end
+    resources :like_reviews, only: :create
+    resources :watchlists, only: [:create, :destroy]
   end
-  
-  namespace :admin do
-    resources :users
-    resources :movies
-    resources :reviews
-    resources :comments
-    resources :genres
-    resources :descriptions
-  end
-  resources :like_reviews, only: :create
-  resources :watchlists, only: [:create, :destroy]
 end
