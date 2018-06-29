00881e1d74c992291bff4b772fef811b4eff2ed5
giangmt <maitruonggiangbkhn@gmail.com>
2018-06-29 20:35:46 +0700

    fix like review

--- app/controllers/like_reviews_controller.rb
+++ app/controllers/like_reviews_controller.rb
@@ -1,19 +1,16 @@
 class LikeReviewsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :load_movie, :load_review
 
   def create
     @like = current_user.like_reviews.build like_params
-    respond_to do |format|
-      if @like.save
+    if @like.save
+      respond_to do |format|
         format.html { redirect_to @like.review.movie }
         format.js
-      else
-        movie = Movie.find_by id: params[:movie_id]
-        format.html do
-          flash[:danger] = t ".not_found"
-          movie ? redirect_to(movie) : redirect_to(root_url)
-        end
       end
+    else
+      flash[:danger] = t ".review_already_liked"
+      redirect_to @movie
     end
   end
 
@@ -21,5 +18,19 @@ class LikeReviewsController < ApplicationController
 
   def like_params
     params.require(:like_review).permit :review_id
+  end
+
+  def load_movie
+    @movie = Movie.find_by id: params[:movie_id]
+    return if @movie
+    flash[:danger] = t ".movie_not_exist"
+    redirect_to root_url
+  end
+
+  def load_review
+    @review = Review.find_by id: like_params[:review_id]
+    return if @review
+    flash[:danger] = t ".review_not_exist"
+    redirect_to @movie
   end
 end
